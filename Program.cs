65ae8f81b8e4d384f3a52fc92adc45e23323829c
Ana Santos <anabia200113@gmail.com>
2023-07-16 19:40:18 -0300

    criacao da requisicao e resposta do servidor, bem como tratativa e erro

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿// estou gerenciando um recurso apenas dentro deste escopo. Qnd fechar libero o recurso client
+
+using (HttpClient client = new HttpClient()){
+    try
+    {
+        string resposta = await client.GetStringAsync($"https://guilhermeonrails.github.io/api-csharp-songs/songs.jso");
+        System.Console.WriteLine(resposta);     
+    } 
+    catch(Exception ex)
+    {
+        //throw ex;
+        System.Console.WriteLine($"Falha ao requisitar os dados à API externa. Erro: {ex.Message}"); 
+    } 
+}
+
